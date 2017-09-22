7283102fb5a1c937452c7a00f72198a558cbcd05
Andy Hall <anthrond@gmail.com>
2017-09-21 23:57:11 -0600

    Add variant of cp-diffed-configs.sh that takes arbitrary command

---
+++ xargs-diffed-commits.sh
@@ -0,0 +1,12 @@
+for i in $(grep -i '#\s*\(place\|put\) in' . -r | sed 's/^\([^:]*\):\s*#\s*[Pp]\(lace\|ut\)\s*in\s*\([^[:space:]]*\).*/\1:\3/')
+do 
+    git_path=$(echo $i | sed 's/\([^:]*\):\(.*\)/\1/')
+    etc_path=$(echo $i | sed 's/\([^:]*\):\(.*\)/\2/')$(basename $git_path)
+    diff $git_path $etc_path >/dev/null
+    is_diff=$?
+    if [ $is_diff -ne 0 ]
+    then
+        echo $1 $etc_path $git_path
+        $1 $etc_path $git_path
+    fi
+done
